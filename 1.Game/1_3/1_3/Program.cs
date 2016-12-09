6109d0aef8f65414964863b017948311f51c9b89
zhangxiaoya <tiyijushi@163.com>
2016-12-10 00:17:44 +0800

    :rocket: add a visual project for algorithm 3 and modify git ignore file

---
+++ 1.Game/1_3/1_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace _1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MakeUsage((float) 0.5);
+        }
+
+        private static void MakeUsage(float level)
+        {
+            PerformanceCounter performanceCounter = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+
+            while (true)
+            {
+                if (performanceCounter.NextValue() > level)
+                    System.Threading.Thread.Sleep(10);
+            }
+        }
+    }
+}
